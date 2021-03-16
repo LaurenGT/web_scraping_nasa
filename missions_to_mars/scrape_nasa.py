f4bb2bd346fc6a396f9892396b75eb5451cec9c1
LaurenGT <ltoothaker2@gmail.com>
2021-03-15 19:07:16 -0700

    adding time.sleep

--- missions_to_mars/scrape_nasa.py
+++ missions_to_mars/scrape_nasa.py
@@ -1,14 +1,17 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 def scrape():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
+    time.sleep(10)
+
     articles = {}
 
-    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    url = "https://mars.nasa.gov/news/"
     browser.visit(url)
 
     html = browser.html
