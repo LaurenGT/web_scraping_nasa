e344bf24a5d319a26a36a9a60c099779c33245f3
LaurenGT <ltoothaker2@gmail.com>
2021-03-15 17:20:30 -0700

    loads page. Returns errors when attempting to scrape.

--- missions_to_mars/app.py
+++ missions_to_mars/app.py
@@ -4,6 +4,20 @@ import scrape_nasa
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    marsArticle = mongo.db.mars.find_one()
+    return render_template('index.html', listings=marsArticle)
+
+@app.route('/scrape')
+def scraper():
+    articles = mongo.db.mars.find_one()
+    data = scrape_nasa.scrape()
+    articles.update({}, data, upsert=True)
+    return redirect(url_for("index"), code=302)
 
 
 if __name__ == "__main__":
--- missions_to_mars/scrape_nasa.py
+++ missions_to_mars/scrape_nasa.py
@@ -1,3 +1,25 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 
+def scrape():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    articles = {}
+
+    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    articles['title'] = soup.find("div", class_="content_title").text
+    articles['teaser'] = soup.find("div", class_="article_teaser_body").text
+    # article_image = soup.find_one("div", class_="list_image")
+
+    # print(article_title)
+
+    browser.quit
+
+    return articles
