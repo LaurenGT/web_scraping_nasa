21cca48d1ba3ab50f5b2c2df3614bca87bc6abe1
LaurenGT <ltoothaker2@gmail.com>
2021-03-21 12:45:27 -0700

    scrapes all info and loads to webpage through flask app. working on styles

--- missions_to_mars/scrape_nasa.py
+++ missions_to_mars/scrape_nasa.py
@@ -2,6 +2,7 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import pandas as pd
 
 def scrape():
     executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -15,6 +16,8 @@ def scrape():
     news_url = "https://mars.nasa.gov/news/"
     browser.visit(news_url)
 
+    time.sleep(5)
+
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
 
@@ -26,90 +29,111 @@ def scrape():
     featured_image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
     browser.visit(featured_image_url)
 
-    visit_image = browser.links.find_by_partial_href("featured").click()
-    base_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/"
+    time.sleep(5)
 
-    html=browser.html
-    soup=BeautifulSoup(html,"html.parser")
+    visit_image = browser.links.find_by_partial_href("featured").click()
+    base_img_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/"
+
+    html_img=browser.html
+    soup=BeautifulSoup(html_img,"html.parser")
     image = soup.find('img', class_='fancybox-image')
     src_img = image['src']
-    reference_img_url = base_url + src_img
+    reference_img_url = base_img_url + src_img
     mars_info['reference_img_url'] = reference_img_url
 
     # scrape for cerberus hemisphere image
     hem_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(hem_url)
 
-    visit_cerb = browser.links.find_by_partial_text("Cerberus").click()
-    base_url = "https://astrogeology.usgs.gov/"
+    time.sleep(5)
 
-    html=browser.html
-    soup=BeautifulSoup(html,"html.parser")
+    visit_cerb = browser.links.find_by_partial_text("Cerberus").click()
+    base_hem_url = "https://astrogeology.usgs.gov/"
+
+    html_cerb=browser.html
+    soup=BeautifulSoup(html_cerb,"html.parser")
     cerb_image = soup.find('img', class_='wide-image')
     src_img = cerb_image['src']
-    cerb_url = base_url + src_img
+    cerb_url = base_hem_url + src_img
     mars_info['cerb_hem'] = cerb_url
 
     # scrape for schiaparelli hemisphere image
     hem_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(hem_url)
 
-    visit_schi = browser.links.find_by_partial_text("Schiaparelli").click()
-    base_url = "https://astrogeology.usgs.gov/"
+    time.sleep(5)
 
-    html=browser.html
-    soup=BeautifulSoup(html,"html.parser")
+    visit_schi = browser.links.find_by_partial_text("Schiaparelli").click()
+    base_hem_url = "https://astrogeology.usgs.gov/"
+
+    html_schi=browser.html
+    soup=BeautifulSoup(html_schi,"html.parser")
     schi_image = soup.find('img', class_='wide-image')
     src_img = schi_image['src']
-    schi_url = base_url + src_img
+    schi_url = base_hem_url + src_img
     mars_info['schi_hem'] = schi_url
 
     # scrape for syrtis major hemisphere image
     hem_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(hem_url)
 
-    visit_syr = browser.links.find_by_partial_text("Syrtis").click()
-    base_url = "https://astrogeology.usgs.gov/"
+    time.sleep(5)
 
-    html=browser.html
-    soup=BeautifulSoup(html,"html.parser")
+    visit_syr = browser.links.find_by_partial_text("Syrtis").click()
+    base_hem_url = "https://astrogeology.usgs.gov/"
+
+    html_syr=browser.html
+    soup=BeautifulSoup(html_syr,"html.parser")
     syr_image = soup.find('img', class_='wide-image')
     src_img = syr_image['src']
-    syr_url = base_url + src_img
+    syr_url = base_hem_url + src_img
     mars_info['syr_hem'] = syr_url
 
     # scrape for valles marineris hemisphere image
     hem_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(hem_url)
 
-    visit_vall = browser.links.find_by_partial_text("Valles").click()
-    base_url = "https://astrogeology.usgs.gov/"
+    time.sleep(5)
 
-    html=browser.html
-    soup=BeautifulSoup(html,"html.parser")
+    visit_vall = browser.links.find_by_partial_text("Valles").click()
+    base_hem_url = "https://astrogeology.usgs.gov/"
+
+    html_vall=browser.html
+    soup=BeautifulSoup(html_vall,"html.parser")
     vall_image = soup.find('img', class_='wide-image')
     src_img = vall_image['src']
-    vall_url = base_url + src_img
+    vall_url = base_hem_url + src_img
     mars_info['vall_hem'] = vall_url
 
     # scrape info table
     facts_url = "https://space-facts.com/mars/"
     browser.visit(facts_url)
 
-    html=browser.html
-    soup=BeautifulSoup(html,"html.parser")
+    time.sleep(5)
+
+    html_table=browser.html
+    soup=BeautifulSoup(html_table,"html.parser")
 
     facts_table = soup.find_all('table', class_="tablepress tablepress-id-p-mars")[0]('tr')
 
+    # ultimate_facts = dict()
     key=[]
     value=[]
     for tr in facts_table:
         col_one = tr.find('td', class_="column-1").text
         col_two = tr.find('td', class_="column-2").text
+        # ultimate_facts[col_one] = col_two
         key.append(col_one)
         value.append(col_two)
 
-    mars_info["data_zip"] = list(zip(key,value))
+    # ultimate_df = pd.DataFrame(ultimate_facts)
+    # ultimate_df.columns=['Description','Mars']
+    # ultimate_df.set_index('Description', inplace=False)
+    mars_info["data_zip"] = dict(zip(key,value))
+    # mars_info["data_zip"] = ultimate_df.to_html(classes="table table-striped")
+    # print(mars_info["data_zip"])
+
+    browser.quit()
 
     return mars_info
-    browser.quit()
+    
